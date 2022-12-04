43a0a523aeb9f989f890b3a42d3db8bf4b7b113f
valentinstoecker <valentin@stoeckermail.de>
2022-12-04 15:48:03 +0100

    check duplicate name

--- main.go
+++ main.go
@@ -22,6 +22,15 @@ func has_id(es []entry, b byte) bool {
 	return false
 }
 
+func has_name(es []entry, name string) bool {
+	for _, e := range es {
+		if e.name == name {
+			return true
+		}
+	}
+	return false
+}
+
 func permutate_rand(es []entry) []entry {
 	es_copy := make([]entry, len(es))
 	copy(es_copy, es)
@@ -40,6 +49,16 @@ func main() {
 		if name == "" {
 			return
 		}
+
+		for _, e := range objs {
+			if e.name == name {
+				obs := make([]byte, 2)
+				hex.Encode(obs, []byte{e.id})
+				w.Write(obs)
+				return
+			}
+		}
+
 		bs := make([]byte, 1)
 		for {
 			rand.Read(bs)
@@ -56,9 +75,16 @@ func main() {
 
 	http.HandleFunc("/result", func(w http.ResponseWriter, r *http.Request) {
 		es := permutate_rand(objs)
+		es_copy := make([]entry, len(es))
+		copy(es_copy, es)
+		for i := range es {
+			es_copy[i].id = es[(i+1)%len(es)].id
+			es_copy[i].name = es[i].name
+		}
+		permutate_rand(es_copy)
 		str := ""
-		for i, e := range es {
-			str += e.name + " " + hex.EncodeToString([]byte{es[(i+1)%len(es)].id}) + "\n"
+		for _, e := range es_copy {
+			str += fmt.Sprintf("%s: %x\n", e.name, e.id)
 		}
 		w.Write(bytes.NewBufferString(str).Bytes())
 	})
